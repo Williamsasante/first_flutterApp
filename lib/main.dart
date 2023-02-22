9b91c1c39ccaf14c7c6501e113f3bf8b90c8128e
William Amoako Asante <a_williams55@yahoo.com>
2023-02-22 13:35:12 +0000

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class FirstApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Portfolio',
       home: Scaffold(
           backgroundColor: Color.fromARGB(255, 255, 255, 255),
@@ -17,7 +18,7 @@ class FirstApp extends StatelessWidget {
             backgroundColor: Color.fromARGB(255, 28, 51, 114),
             title: Align(
               alignment: Alignment.center,
-              child: Text("PORTFOLIO"),
+              child: Text("HOME"),
             ),
           ),
           body: Column(
@@ -94,6 +95,64 @@ class FirstApp extends StatelessWidget {
                         icon: Icon(Icons.link),
                         iconColor: Color.fromARGB(255, 32, 55, 92)),
                   ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 30, top: 25, right: 30),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 166, 178, 219),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Facebook",
+                        icon: Icon(Icons.facebook_sharp),
+                        iconColor: Color.fromARGB(255, 32, 55, 92)),
+                  ),
+                ),
+              ),
+              //Textbutton
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(
+                      Color.fromARGB(255, 255, 255, 255)),
+                  backgroundColor: MaterialStateProperty.all(
+                      Color.fromARGB(255, 28, 51, 114)),
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Update",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+
+              ElevatedButton(
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(
+                      Color.fromARGB(255, 255, 255, 255)),
+                  backgroundColor: MaterialStateProperty.all(
+                      Color.fromARGB(255, 36, 197, 0)),
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Save",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              OutlinedButton(
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(
+                      Color.fromARGB(255, 255, 255, 255)),
+                  backgroundColor:
+                      MaterialStateProperty.all(Color.fromARGB(255, 232, 0, 0)),
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Delete",
+                  style: TextStyle(fontSize: 20),
                 ),
               ),
             ],
